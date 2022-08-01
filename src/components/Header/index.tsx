a83fd37dc31d379d066b71861a27c244dad3e003
Luiz Preto <103388468+Luizth25@users.noreply.github.com>
2022-08-01 15:23:42 -0300

    project started, folders created, homepage and routes added

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>GitHub User Search</h1>
+      </header>
+      <main>
+        <input type="text" />
+        <button>Search</button>
+      </main>
+    </>
+  );
+};
+export default Header;
