bde3f3ae76581d9e8c0f34b89da41c73b41f5b3b
Luiz Preto <103388468+Luizth25@users.noreply.github.com>
2022-08-01 20:54:22 -0300

    added a theme for the project and also the global css and reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import Routes from "./routes";
+import theme from "./styles/theme";
+import GlobalStyle from "./styles/globals";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,35 @@
+import { DefaultTheme } from "styled-components";
+
+type TFontStyle = {
+  fontSize: number;
+  fontWeight: number;
+};
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: { [key in keyof typeof themeColors]: string };
+    fontStyles: { [key in keyof typeof themeFontStyles]: TFontStyle };
+  }
+}
+
+const themeColors = {
+  background: "#faf6ed",
+  fontColor: "#222",
+  containerColor: "#FB5",
+};
+
+const themeFontStyles = {
+  paragraph: {
+    fontWeight: 400,
+    fontSize: 16,
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight: 600,
+  },
+};
+const theme: DefaultTheme = {
+  colors: themeColors,
+  fontStyles: themeFontStyles,
+};
+export default theme;
